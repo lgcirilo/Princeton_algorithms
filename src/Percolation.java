b1304af1c2b3db3597f1005a89ee74192b47606a
Luiz Gustavo Santos Cirilo <lgcirilo@gmail.com>
2018-08-06 13:00:38 -0300

    Add files via upload
    Apparently finished. Further testing required.

--- src/Percolation.java
+++ src/Percolation.java
@@ -2,25 +2,84 @@
  * Created by cyfa on 26/12/2017.
  */
 
+//Throw a java.lang.IllegalArgumentException if any argument to open(), isOpen(), or isFull()
+
 import edu.princeton.cs.algs4.*;
 
 public class Percolation extends WeightedQuickUnionUF{
 
-    boolean[] opened;
+    private boolean[] opened;
+    private int n; //row/column size
 
-    public Percolation (int n) { // create n-by-n grid, with all sites blocked
-        super(n*n);
-        opened = new boolean[n*n];
-        for (int i = 0; i < n*n; i++) {
-                opened[i] = false;
+    public Percolation (int n) { // create n-by-n grid, with all sites blocked, plus two extra virtual elements
+        super(n * n + 2);
+        if (n <= 0){
+            throw new IllegalArgumentException("Size must be greater than 0");
+        } else {
+            this.n = n;
+            opened = new boolean[n * n + 2];
+            for (int i = 1; i <= n; i++) {
+                int virtualTop = n * n;
+                int virtualBottom = n * n + 1;
+                int currentTopRowSite = xyTo1dIndex(1, i);
+                int currentBottomRowSite = xyTo1dIndex(n, i);
+                union(virtualTop, currentTopRowSite);
+                union(virtualBottom, currentBottomRowSite);
+            }
         }
     }
 
-    //TODO - check for array out of bounds
+    //TODO - check for array out of bounds???
     public void open(int row, int col) { // open site (row, col) if it is not open already
+        System.out.println("abrindo (" + row + "," + col + ")");
+
         int index = xyTo1dIndex(row, col);
         if (!opened[index]) {
             opened[index] = true;
+            //check neighbouring sites (top, bottom, left, right) and connect if opened.
+            //chekcking (row, col - 1)
+            if (row >= 1 && row <= n && col - 1 >= 1 && col - 1 <= n) {
+                int neighborIndex = xyTo1dIndex(row, col -1);
+
+                if (isOpen(row, col -1)) {
+                  if (!connected(index,neighborIndex)) {
+                      union(index, neighborIndex);
+                  }
+                }
+            }
+
+            //chekcking (row, col + 1)
+            if (row >= 1 && row <= n && col + 1 >= 1 && col + 1 <= n) {
+
+                int neighborIndex = xyTo1dIndex(row, col + 1);
+                if (isOpen(row, col + 1)) {
+                    if (!connected(index,neighborIndex)) {
+                        union(index, neighborIndex);
+                    }
+                }
+            }
+
+            //chekcking (row - 1, col)
+            if (row - 1 >= 1 && row - 1 <= n && col >= 1 && col <= n) {
+
+                int neighborIndex = xyTo1dIndex(row - 1, col);
+                if (isOpen(row - 1, col)) {
+                    if (!connected(index,neighborIndex)) {
+                        union(index, neighborIndex);
+                    }
+                }
+            }
+
+            //checking (row + 1 , col)
+            if (row + 1 >= 1 && row + 1 <= n && col >= 1 && col <= n) {
+
+                int neighborIndex = xyTo1dIndex(row + 1, col);
+                if (isOpen(row + 1, col)) {
+                    if (!connected(index,neighborIndex)) {
+                        union(index, neighborIndex);
+                    }
+                }
+            }
         }
     }
 
@@ -33,15 +92,17 @@ public class Percolation extends WeightedQuickUnionUF{
 
     //TODO - not yet implemented
     public boolean isFull(int row, int col) { // is site (row, col) full?
-        boolean result = true; // dummy value. check what to initialize it with in finished project
-
-        return result;
+        int OneDCoordinate = xyTo1dIndex(row,col);
+        for (int i = 0; i < n; i++) { //loops through top row
+            if ( connected(OneDCoordinate, i)) return true;
+        }
+        return false;
     }
 
 
     public int numberOfOpenSites() {     // number of open sites
         int result = 0; // dummy value. check what to initialize it with in finished project
-        for (int i = 0; i < count(); i++) {
+        for (int i = 0; i < n*n; i++) {
             if (opened[i]) result++;
         }
 
@@ -51,44 +112,87 @@ public class Percolation extends WeightedQuickUnionUF{
 
     //TODO - not yet implemented
     public boolean percolates() {      // does the system percolate?
-        boolean result;
-        int rowLength = (int)Math.sqrt(this.count());
-        int startIndex = count() - rowLength;
-
-
-        return result;
+        for (int i = 1; i <= n; i++) { //loops through bottom row
+            if (isFull(n,i)) { return true; }
+        }
+        return false;
     }
 
-
+    //accessory methods from now on. Should all be private.
     private int xyTo1dIndex(int row, int col) { // converts from x,y coordinates to WeightedQuickUnionUF 1d array index
         int index;
-        int rowLength = (int)Math.sqrt(this.count());
-        index = (rowLength * (row - 1)) + (col - 1);
+        if (row < 1 || row > n || col < 1 || col > n) {
+            throw new IllegalArgumentException("index (" + row + "," + col + ") is not between (1,1) and " + "(" + n + "," + n + ")");
+        } else {
+            index = (this.n * (row - 1)) + (col - 1);
+        }
         return index;
-
     }
 
     // test client (optional)
     public static void main(String[] args) {
         //tests constructor
         Percolation p = new Percolation(10);
+//        p.size();
         //tests xyTo1dIndex(int row, int col)
-        System.out.println(p.xyTo1dIndex(1,1));
-        System.out.println(p.xyTo1dIndex(10,10));
-        System.out.println(p.xyTo1dIndex(2,2));
-        System.out.println(p.xyTo1dIndex(5,3));
-        //tests open(int row, int col)
-        p.open(1,1);
-        p.open(10,10);
-        p.open(2,2);
-        p.open(5, 3);
-        System.out.println(p.numberOfOpenSites());
-        //tests isOpen(int row, int col)
-        System.out.println(p.isOpen(1,1));
-        System.out.println(p.isOpen(10,10));
-        System.out.println(p.isOpen(5,3));
-        System.out.println(p.isOpen(3,4));
-        System.out.println(p.isOpen(9,8));
+//        System.out.println(p.xyTo1dIndex(1,1));
+//        System.out.println(p.xyTo1dIndex(10,10));
+//        System.out.println(p.xyTo1dIndex(2,2));
+//        System.out.println(p.xyTo1dIndex(5,3));
+//        tests open(int row, int col)
+//        p.open(1,1);
+//        p.open(10,10);
+//        p.open(2,2);
+//        p.open(1,2);
+//        p.open(5, 3);
+//        p.open(0,0);
+//        p.open(0,1);
+//        p.open(1,0);
+//        p.open(11,1);
+//        p.open(1,11);
+//        p.open(1,1);
+//        p.open(1,2);
+//        p.open(2,2);
+//        p.open(3,2);
+//        p.open(4,2);
+        p.open(1, 1);
+        System.out.println("percolates? " + p.percolates());
+        p.open(2,1);
+        System.out.println("percolates? " + p.percolates());
+        p.open(3,1);
+        System.out.println("percolates? " + p.percolates());
+        p.open(4,1);
+        System.out.println("percolates? " + p.percolates());
+        p.open(5,1);
+        System.out.println("percolates? " + p.percolates());
+        p.open(6,1);
+        System.out.println("percolates? " + p.percolates());
+        p.open(7,1);
+        System.out.println("percolates? " + p.percolates());
+        p.open(8,1);
+        System.out.println("percolates? " + p.percolates());
+        p.open(9,1);
+        System.out.println("percolates? " + p.percolates());
+        p.open(10,1);
+        System.out.println("percolates? " + p.percolates());
 
+
+
+        System.out.println("open site count: " + p.numberOfOpenSites());
+
+//        tests isOpen(int row, int col)
+//        System.out.println(p.isOpen(1,1));
+//        System.out.println(p.isOpen(10,10));
+//        System.out.println(p.isOpen(5,3));
+//        System.out.println(p.isOpen(3,4));
+//        System.out.println(p.isOpen(9,8));
+        //1dcoords
+//        int first = p.xyTo1dIndex(1,1);
+//        int second = p.xyTo1dIndex(4,2);
+//        int first = 101;
+//        int second = 100;
+//        System.out.println("pontos conectados? " + p.connected(first,second));
+//        System.out.println("isFull?: " + p.isFull(6,1));
+//        System.out.println("isFull?: " + p.isFull(5,5));
     }
 }
