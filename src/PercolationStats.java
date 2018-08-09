582738e1b765fc4c28805ecb2af3cd1d242aa640
Luiz Gustavo Santos Cirilo <lgcirilo@gmail.com>
2018-08-09 12:30:21 -0300

    Update PercolationStats.java
    Constructor now throw exception if n or trials are less than or equal to zero.

--- src/PercolationStats.java
+++ src/PercolationStats.java
@@ -9,18 +9,22 @@ public class PercolationStats {
 
     // Constructor. Performs independent experiments on an n-by-n grid
     public PercolationStats(int n, int trials) {
-        gridSize = n;
-        numTrials = trials;
-        openedSitesPercentagePerTrial = new double[trials];
+        if (n <=0 || trials <= 0) {
+            throw new IllegalArgumentException("grid size and number of trials cannot be smaller than one");
+        } else {
+            gridSize = n;
+            numTrials = trials;
+            openedSitesPercentagePerTrial = new double[trials];
 
-        for (int i = 0; i < numTrials; i++) {
-            Percolation p = new Percolation(gridSize);
-            while (!p.percolates()) {
-                int rowToOpen = (int) Math.ceil(StdRandom.uniform() * gridSize);
-                int colToOpen = (int) Math.ceil(StdRandom.uniform() * gridSize);
-                p.open(rowToOpen, colToOpen);
+            for (int i = 0; i < numTrials; i++) {
+                Percolation p = new Percolation(gridSize);
+                while (!p.percolates()) {
+                    int rowToOpen = (int) Math.ceil(StdRandom.uniform() * gridSize);
+                    int colToOpen = (int) Math.ceil(StdRandom.uniform() * gridSize);
+                    p.open(rowToOpen, colToOpen);
+                }
+                openedSitesPercentagePerTrial[i] = p.numberOfOpenSites() / Math.pow(gridSize, 2);
             }
-            openedSitesPercentagePerTrial[i] = p.numberOfOpenSites() / Math.pow(gridSize, 2);
         }
     }
 
