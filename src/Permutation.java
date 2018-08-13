aad693ecb696c7560ff1226be8dab786f757c2eb
cyfa <cyfa@petrobras.com.br>
2018-08-13 15:32:06 -0300

    Starting week 2: Deque and RandomizedDeque. Class APIs only.

---
+++ src/Permutation.java
@@ -0,0 +1,6 @@
+/**
+ * Created by cyfa on 13/08/2018.
+ */
+public class Permutation {
+    public static void main(String[] args) {}
+}
---
+++ src/RandomizedQueue.java
@@ -0,0 +1,34 @@
+/**
+ * Created by cyfa on 13/08/2018.
+ */
+
+import java.util.Iterator;
+
+//Use resizing arrays - constant amortized time.
+
+public class RandomizedQueue<Item> implements Iterable<Item> {
+    
+    // construct an empty randomized queue
+    public RandomizedQueue() {}
+
+    // is the randomized queue empty?
+    public boolean isEmpty() {}
+
+    // return the number of items on the randomized queue
+    public int size() {}
+
+    // add the item
+    public void enqueue(Item item) {}
+
+    // remove and return a random item
+    public Item dequeue() {}
+
+    // return a random item (but do not remove it)
+    public Item sample() {}
+
+    // return an independent iterator over items in random order
+    public Iterator<Item> iterator() {}
+
+    // unit testing (optional)
+    public static void main(String[] args) {}
+}
