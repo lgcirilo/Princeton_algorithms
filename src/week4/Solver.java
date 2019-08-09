814ce2c033a3bc3afb4ba8d4dae7bc7688fc84bf
cyfa <cyfa@petrobras.com.br>
2019-08-09 10:50:15 -0300

    week 4 start. 8 puzzle. Some more code to solver. Little progress.

--- src/week4/Solver.java
+++ src/week4/Solver.java
@@ -6,37 +6,79 @@ import java.util.Comparator;
 
 public class Solver {
 
+    private Board previous = null;
     private int moves = 0;
     private MinPQ<Board> boardMinPQ = new MinPQ<>(new BoardComparatorHamming());
-    private ArrayList<Board> solution = new ArrayList<>();
-
+//    private ArrayList<Board> solution = new ArrayList<>();
+    private ArrayList<Board> boardsOnPreviousPass = new ArrayList<>();
+//    private ArrayList<Board> temp = new ArrayList<>();
+//    int[][] arr1 = {{1,0,3},{4,2,5},{7,8,6}};
+//    int[][] arr2 = {{4,1,3},{0,2,5},{7,8,6}};
+//    int[][] arr3 = {{2,4,0},{1,3,7},{6,8,5}};
+//    int[][] arr4 = {{1,4,7},{2,5,8},{3,6,0}};
+//    Board b1 = new Board(arr1);
+//    Board b2 = new Board(arr2);
+//    Board b3 = new Board(arr3);
+//    Board b4 = new Board(arr4);
 
     // find a solution to the initial board (using the A* algorithm)
-    public Solver(Board initial){
+    public Solver(Board initial) {
 
         if (initial == null) {
             throw new IllegalArgumentException();
         }
 
         boardMinPQ.insert(initial);
+//        boardsOnPreviousPass.add(initial);
+        previous = boardMinPQ.delMin();
+        System.out.println(previous.toString());
 
-        for (Board board: initial.neighbors()) {
-            boardMinPQ.insert(board);
-        }
 
-        for (Board board: boardMinPQ) {
+//        while (!foundGoal()) {
+//            for (Board board : boardsOnPreviousPass) {
+//                for (Board b : board.neighbors()) {
+//                    temp.add(b);
+//                }
+//            }
+//            for (Board board: temp) {
+//                boardsOnPreviousPass.add(board);
+//                boardMinPQ.insert(board);
+//            }
+//
+//            temp = new ArrayList<>();
+//        }
+
+
+
+
+//        for (Board parentBoard: boardsOnPreviousPass) {
+//            for (Board neighborBoard: parentBoard.neighbors())
+//            boardMinPQ.insert(neighborBoard);
+//        }
+//
+        for (Board board : boardMinPQ) {
             System.out.println(board);
         }
 
-        // this is just to test the hamming and manhattan comparators. Delete after done.
-        int[][] boardA = {{1,0,3},{4,2,5},{7,8,6}};
-        int[][] boardB = {{4,1,3},{0,2,5},{7,8,6}};
-        Board a = new Board(boardA);
-        Board b = new Board(boardB);
-        System.out.println(new BoardComparatorHamming().compare(a, b));
-        System.out.println(new BoardComparatorManhattan().compare(a, b));
-        // this is just to test the hamming and manhattan comparators Delete after done.
 
+//        boardMinPQ.insert(b1);
+//        boardMinPQ.insert(b3);
+//        boardMinPQ.insert(b2);
+//        boardMinPQ.insert(b4);
+//
+//        solution.add(boardMinPQ.delMin());
+//        solution.add(boardMinPQ.delMin());
+//        System.out.println(solution.get(0));
+//        System.out.println(solution.get(1));
+
+
+    }
+
+    private boolean foundGoal() {
+        for (Board board: boardsOnPreviousPass) {
+            if (board.isGoal()) {return true;}
+        }
+        return false;
     }
 
     // is the initial board solvable? (see below){
